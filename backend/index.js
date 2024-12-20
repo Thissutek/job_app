da562901ed4b4d24351e531c3e78a80e4bae2790
unknown <jonathan_yau@outlook.com>
2024-12-19 19:48:40 -0500

    Table is displaying and inserting into DB correctly

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello from the Backend')
-})
-
 //Sign Up Route
 app.post('/api/auth/signup', async (req, res) => {
     const { username, email, password } = req.body;
@@ -43,7 +39,7 @@ app.post('/api/auth/signup', async (req, res) => {
   }); 
 
 
-// Login
+// Login Route
 app.post('/api/auth/login', async (req, res) => {
   const { email, password } = req.body;
 
@@ -75,6 +71,48 @@ app.post('/api/auth/login', async (req, res) => {
     res.status(500).json({ message: 'Internal Server Error' });
   }
 });
+
+// Add new job application to table
+app.post('/api/jobs', authenticateToken, async (req, res) => {
+  const { jobTitle, company, salary, dateApplied, status, interviewStage} = req.body;
+  const {userId} = req.user
+
+  if(!userId) {
+    return res.status(401).json({ message: "Unauthorized"})
+  }
+
+  try {
+    const query = `INSERT INTO job_applications (user_id, job_title, company, salary, date_applied, status, interview_stage) 
+    VALUES($1, $2, $3, $4, $5, $6, $7)
+    RETURNING *;
+    `;
+    const values = [userId, jobTitle, company, salary, dateApplied, status, interviewStage];
+    await pool.query(query, values)
+  } catch(error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error'})
+  }
+
+});
+
+//Retrieve job list for existing user
+app.get('/api/jobs', authenticateToken, async (req, res) => {
+  if(!req.user) {
+    return res.status(401).json({ messaage: 'Unauthorized'});
+  }
+  
+  const userId = req.user.userId;
+
+  try {
+    const query = 'SELECT * FROM job_applications WHERE user_id = $1;'
+    const result = await pool.query(query, [userId])
+
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error('Error fetching job applications', error);
+    res.status(500).json({ message: 'Server Error. Please try again.'})
+  }
+})
 
 
 app.listen(port, () => {
